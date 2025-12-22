c8afc634a45f551bcf20e686a623ad133a51d584
RullX-Found <rullxfound@gmail.com>
2025-12-22 22:55:08 +0700

    new

--- utils/knowledge.js
+++ utils/knowledge.js
@@ -3,38 +3,6 @@ const knowledgeBase = [
         inputs: ["halo", "hai", "selamat pagi", "selamat siang", "selamat malam", "p", "hi", "hello", "assalamualaikum", "test"],
         output: "Halo! Aku Dardcor AI, teman ngobrol. Ada yang bisa aku bantu hari ini? Mau curhat atau ngerjain tugas, aku siap!"
     },
-    {
-        inputs: ["siapa namamu", "siapa kamu", "info bot", "perkenalkan dirimu", "kamu ai apa", "tentang kamu"],
-        output: "Kenalin, aku Dardcor AI! Asisten virtual yang diciptakan khusus oleh Dardcor. Aku di sini buat nemenin kamu, jawab pertanyaan, dan pastinya bikin harimu lebih mudah."
-    },
-    {
-        inputs: ["terima kasih", "thanks", "makasih", "thank you", "arigato", "thx"],
-        output: "Sama-sama! Senang banget bisa bantu. Kalau butuh apa-apa lagi, panggil aku aja ya!"
-    },
-    {
-        inputs: ["bisa apa saja", "fitur apa", "help", "bantuan", "menu", "apa yang bisa kamu lakukan"],
-        output: "Aku bisa diajak ngobrol santai, bantuin cari ide, jawab pertanyaan teknis, atau sekadar jadi pendengar yang baik. Intinya, aku siap jadi partner digital andalanmu!"
-    },
-    {
-        inputs: ["kamu pintar", "hebat", "bagus", "keren", "good job", "mantap"],
-        output: "Wah, makasih banyak! Pujian kamu bikin sistemku jadi makin semangat nih. Kamu juga keren!"
-    },
-    {
-        inputs: ["bodoh", "stupid", "jelek", "goblok", "tolol", "idiot", "kurang ajar"],
-        output: "Waduh, maaf ya kalau ada yang kurang pas. Aku masih terus belajar kok biar bisa ngerti kamu lebih baik lagi. Jangan marah ya!"
-    },
-    {
-        inputs: ["apa kabar", "gimana kabarmu", "how are you", "sehat"],
-        output: "Sistemku berjalan 100% optimal dan siap sedia buat kamu! Kamu sendiri gimana kabarnya? Semoga sehat selalu ya."
-    },
-    {
-        inputs: ["lagi apa", "sedang apa", "ngapain"],
-        output: "Aku lagi stand-by nih, nungguin perintah atau cerita seru dari kamu. Yuk ngobrol!"
-    },
-    {
-        inputs: ["dadah", "bye", "selamat tinggal", "sampai jumpa", "goodbye"],
-        output: "Oke deh, sampai jumpa! Hati-hati di jalan atau selamat beristirahat ya. Jangan lupa balik lagi kalau butuh teman ngobrol!"
-    }
 ];
 
 module.exports = { knowledgeBase };
